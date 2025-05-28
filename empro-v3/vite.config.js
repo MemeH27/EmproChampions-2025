349a872003403db044d64235d5672bd9c008fa9f
Elmer Hernandez <yayirobe2305@gmail.com>
2025-05-27 20:35:52 -0600

    PRUEBA 7

--- empro-v3/vite.config.js
+++ empro-v3/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/EmproChampions-2025/',
   plugins: [react()],
-})
+  base: '/EmproChampions-2025/', // <- Asegurate de poner el nombre del repo aquí
+});
